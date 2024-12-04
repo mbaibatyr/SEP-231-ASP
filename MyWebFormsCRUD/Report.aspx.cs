2f76721598a64fe18ba9d72c4e9c62c23bb5b9b2
mbaibatyr <murat_b@mail.ru>
2024-12-04 20:59:40 +0500

    +

--- MyWebFormsCRUD/Report.aspx.cs
+++ MyWebFormsCRUD/Report.aspx.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Drawing.Charts;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,8 +13,38 @@ namespace MyWebFormsCRUD
 {
     public partial class Report : System.Web.UI.Page
     {
+        DataTable GetData()
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection db = new SqlConnection(ConfigurationManager.AppSettings["db"]))
+            {
+                db.Open();
+                using (SqlCommand cmd = new SqlCommand("pGetMusic", db))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    if (cbCategory.SelectedIndex != 0)
+                        cmd.Parameters.AddWithValue("@category_id", cbCategory.SelectedValue);
+                    dt.Load(cmd.ExecuteReader());
+                }
+                db.Close();
+            }
+        }
+        void MakeExcel()
+        {
+
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
+            var p = Request.QueryString["param1"];
+            switch (p)
+            {
+                case "0":
+                    Response.Write("Excel");
+                    break;
+                case "1":
+                    Response.Write("CSV");
+                    break;
+            }
 
         }
     }
