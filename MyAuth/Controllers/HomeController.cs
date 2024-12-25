8a4ca2e015c06e5e81813dc825119a2efdd32e2d
mbaibatyr <murat_b@mail.ru>
2024-12-25 21:14:12 +0500

    +

--- MyAuth/Controllers/HomeController.cs
+++ MyAuth/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using MyAuth.Models;
 using System.Diagnostics;
 
 namespace MyAuth.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
