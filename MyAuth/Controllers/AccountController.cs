6a47411acb8fa36c58286566baec2687fd66cc7f
mbaibatyr <murat_b@mail.ru>
2025-01-08 20:26:31 +0500

    ++

--- MyAuth/Controllers/AccountController.cs
+++ MyAuth/Controllers/AccountController.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
 using MyAuth.Abstract;
 using MyAuth.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 namespace MyAuth.Controllers
 {
+    [Authorize]
     public class AccountController : Controller
     {
         IUser service;
@@ -19,16 +24,26 @@ namespace MyAuth.Controllers
             return View();
         }
 
-        [HttpGet]
+        [HttpGet, AllowAnonymous]
         public IActionResult Login()
         {            
             return View();
         }
 
-        [HttpPost]
-        public IActionResult Login(User model)
+        [HttpPost, AllowAnonymous]
+        public IActionResult Login(SignInRequest model)
         {
-            //var result = service.SignIn(new SignInRequest { login = "user1", psw = "1234+" });
+            var result = service.SignIn(model);
+            if (result)
+            {
+                var claims = new[] { new Claim(ClaimTypes.Name, model.login)};
+                var identity = new ClaimsIdentity(claims,
+                  CookieAuthenticationDefaults.AuthenticationScheme);
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(identity));
+                return Redirect("~/Home/Index");
+            }
+            ModelState.AddModelError("", "Login and/or password is not correct");
             return View();
         }
 
--- MyAuth/Models/SignInRequest.cs
+++ MyAuth/Models/SignInRequest.cs
@@ -1,8 +1,13 @@
-﻿namespace MyAuth.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyAuth.Models
 {
     public class SignInRequest
     {
+        [Required, DisplayName("Логин")]
         public string login { get; set; }
+        [Required, DisplayName("Пароль"), DataType(DataType.Password)]
         public string psw { get; set; }
     }
 }
