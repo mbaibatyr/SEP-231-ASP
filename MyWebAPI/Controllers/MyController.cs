de504f6b512d207c890c43af64404d2d4bbe2d61
mbaibatyr <murat_b@mail.ru>
2025-01-13 21:47:37 +0500

    +

---
+++ MyWebAPI/Controllers/MyController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebAPI.Model;
+using System.Xml.Linq;
+
+namespace MyWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyController : ControllerBase
+    {
+        [HttpGet, Route("Method_1")]
+        public ActionResult Method_1()
+        {
+            return Ok("Hello STEP");
+        }
+
+        [HttpGet, Route("Method_2/{name}/{id}")]
+        public ActionResult Method_2(string name, string id)
+        {
+            return Ok($"Hello {name} - {id}");
+        }
+
+        [HttpPost, Route("Method_3")]
+        public ActionResult Method_3(Book book)
+        {
+            return Ok($"Hello {book.Id} - {book.Name} - {book.Content} - {book.Author}");
+        }
+    }
+}
