befed785f69cd6048521f5bdd91a9e7fd20fb4dd
mbaibatyr <murat_b@mail.ru>
2025-01-20 20:45:58 +0500

    +

--- MyWebAPI/Controllers/MyController.cs
+++ MyWebAPI/Controllers/MyController.cs
@@ -5,6 +5,7 @@ using MyWebAPI.Model;
 using System.Xml.Linq;
 using Dapper;
 using System.Text;
+using System.Net;
 
 namespace MyWebAPI.Controllers
 {
@@ -58,12 +59,66 @@ namespace MyWebAPI.Controllers
         [HttpPost, Route("Method_4_2")]
         public ActionResult Method_4_2(List<Book> books)
         {
-            StringBuilder sb = new StringBuilder();            
+            StringBuilder sb = new StringBuilder();
             foreach (Book book in books)
             {
                 sb.AppendLine($"{book.Name} - {book.Author}");
             }
             return Ok(sb.ToString());
+        }
+
+        [HttpPost, Route("Method_5/{id}")]
+        public ActionResult Method_5(string id, AuthorMain model)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var book in model.Books)
+            {
+                sb.AppendLine($"{book.Name} - {model.Author}");
+            }
+            sb.AppendLine($"{id} - {id}");
+            return Ok(sb.ToString());
+        }
+
+        [HttpGet, Route("Method_6")]
+        public int Method_6(string id)
+        {
+            return int.Parse(id) * int.Parse(id);
+        }
+
+        [HttpGet, Route("Method_7")]
+        public HttpResponseMessage Method_7()
+        {
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+        [HttpPost, Route("Method_8")]
+        public ActionResult Method_8(string[] lst)
+        {
+            return Ok($"{lst.Length}");
+        }
+
+        [HttpGet, Route("Method_9")]
+        public Book Method_9(string Author, string BookName)
+        {
+            return new Book { Name = BookName, Author = Author };
+        }
+
+        [HttpGet, Route("Method_10")]
+        public ActionResult Method_10(string id)
+        {
+            if (id == "1")
+                return Ok();
+            else if (id == "2")
+                return NotFound("не найден");
+            else if (id == "3")
+                return BadRequest("кривые параметры");
+            else if (id == "4")
+                return Unauthorized("не автризваон");
+            else if (id == "5")
+                return NotFound("не найден");
+            else if (id == "6")
+                return NoContent();
+            else return Ok("ok");
         }
     }
 }
--- MyWebAPI/Model/Book.cs
+++ MyWebAPI/Model/Book.cs
@@ -20,5 +20,11 @@
         public string Name { get; set; }
     }
 
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
 
 }
