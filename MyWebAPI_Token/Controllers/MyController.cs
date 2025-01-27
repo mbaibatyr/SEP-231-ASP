933c8034b91f2701ba24d0f1c61b0493634411bb
mbaibatyr <murat_b@mail.ru>
2025-01-27 20:09:10 +0500

    +

---
+++ MyWebAPI_Token/Controllers/MyController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using MyWebAPI_Token.Model;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MyWebAPI_Token.Controllers
+{
+    [Route("[controller]")]
+    [ApiController, Authorize]
+    public class MyController : ControllerBase
+    {
+        IConfiguration config;
+        public MyController(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+
+        [HttpGet, Route("getDateTime") ]
+        public ActionResult getDateTime()
+        {
+            return Ok(DateTime.Now.ToString("dd.MM.yyyy"));
+        }
+        public ActionResult GetToken(UserModel model)
+        {
+            try
+            {
+
+                /*
+                 проверка в БД
+                 */
+
+                //return Unauthorized(new ReturnStatus
+                //{
+                //    status = StatusEnum.ERROR,
+                //    result = "error"
+                //});
+
+                var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+                var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha512);
+
+                var claims = new[] {
+                      new Claim("myRole", "admin"),
+                      new Claim("dateBirth", "2000-01-01")
+            };
+
+                var token = new JwtSecurityToken(config["Jwt:Issuer"],
+                    config["Jwt:Issuer"],
+                    claims,
+                    //null,
+                    expires: DateTime.Now.AddMinutes(5),
+                    signingCredentials: credentials);
+
+                var sToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+                return Ok(new ReturnStatus
+                {
+                    status = StatusEnum.OK,
+                    result = sToken
+                });
+            }
+            catch (Exception err)
+            {
+                return Ok(new ReturnStatus
+                {
+                    status = StatusEnum.ERROR,
+                    result = "error",
+                    error = err.Message
+                });
+            }
+        }
+    }
+}
---
+++ MyWebAPI_Token/Model/UserModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MyWebAPI_Token.Model
+{
+    public class UserModel
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+    }
+}
