10b36558fced0fb4190772c844e7683e10734ca9
mbaibatyr <murat_b@mail.ru>
2025-01-27 20:10:14 +0500

    +

--- MyWebAPI_Token/Model/UserModel.cs
+++ MyWebAPI_Token/Model/UserModel.cs
@@ -5,4 +5,17 @@
         public string login { get; set; }
         public string password { get; set; }
     }
+
+    public class ReturnStatus
+    {
+        public string result { get; set; }
+        public string error { get; set; }
+        public StatusEnum status { get; set; }
+    }
+
+    public enum StatusEnum
+    {
+        OK = 1,
+        ERROR = 0
+    }
 }
