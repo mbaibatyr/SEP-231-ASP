a134ed5a1bff5b077ccbe8fea7d04d713c9bbf4f
mbaibatyr <murat_b@mail.ru>
2025-02-12 20:38:58 +0500

    ++

--- MyReact/ClientApp/src/components/Home.jsx
+++ MyReact/ClientApp/src/components/Home.jsx
@@ -1,22 +1,59 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
+import { Button, Input, Select } from 'antd';
+import { CheckOutlined } from '@ant-design/icons';
 
 const Home = () => {
+
+  const [loginAdd, setLoginAdd] = useState('123')
+  const [mySelect, setMySelect] = useState('');
+  const handleChangeMySelect = (value) => {
+    setMySelect(value);
+  };
+
+  const MyClick = () => {
+    setLoginAdd('hello step123')
+  }
+
   return (
     <div>
       <h1>Hello, world!</h1>
-      <p>Welcome to your new single-page application, built with:</p>
-      <ul>
-        <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-        <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-        <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-      </ul>
-      <p>To help you get started, we have also set up:</p>
-      <ul>
-        <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-        <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-        <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-      </ul>
-      <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+
+      <Button
+        type='primary'
+        icon={<CheckOutlined />}
+        // onClick={() => {
+        //   alert('hello step')
+        // }}
+        onClick={MyClick}
+      />
+
+      <Input
+        placeholder="Input login"
+        value={loginAdd}
+        onChange={(e) => setLoginAdd(e.target.value)}
+        style={{
+          width: 252, marginLeft: 137
+        }}
+      />
+
+      <Select
+        style={{
+          width: 252, marginLeft: 145
+        }}
+        showSearch
+        status="warning"
+        //defaultValue="2"
+        value={mySelect}
+        optionFilterProp="children"
+        onChange={handleChangeMySelect}
+        filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}
+      >
+        <Option key='4'>BUSINESS ANALYST</Option>
+        <Option key='2'>BUSINESS CUSTOMER</Option>
+        <Option key='3'>CHANGE DEVELOPER</Option>
+        <Option key='5'>DEVELOPER</Option>
+        <Option key='6'>GUEST</Option>
+      </Select>
     </div>
   );
 }
